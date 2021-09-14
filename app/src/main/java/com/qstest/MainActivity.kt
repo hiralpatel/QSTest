0a51902f80657c5895c8f8039865c48792e376d1
hiralpatel <hiral.patel201@gmail.com>
2021-09-15 00:21:13 +0530

    Get value method added

--- app/src/main/java/com/qstest/MainActivity.kt
+++ app/src/main/java/com/qstest/MainActivity.kt
@@ -2,6 +2,7 @@ package com.qstest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 import android.util.Log
 import com.google.firebase.database.*
 import com.qstest.model.Product
@@ -10,6 +11,8 @@ import java.lang.Exception
 
 
 class MainActivity : AppCompatActivity() {
+    private val allProduct: ArrayList<Product> = ArrayList()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,14 +26,14 @@ class MainActivity : AppCompatActivity() {
     private fun getValues(ID: String) {
         val database = FirebaseDatabase.getInstance()
         val refDB = database.reference
-//        val ob = Product(
-//            getVal(refDB, ID, "product-name"), getVal(refDB, ID, "product-desc"),
-//            getVal(refDB, ID, "product-image"), getVal(refDB, ID, "product-price")
-//        )
-//        println("obj: ${ob.name}  ${ob.price} ${ob.desc}  ${ob.img}  ")
+        getVal(refDB, ID, "product-name")
+        getVal(refDB, ID, "product-desc")
+        getVal(refDB, ID, "product-image")
+        getVal(refDB, ID, "product-price")
     }
 
-    private fun getVal(refDB: DatabaseReference, ID: String, key: String) {
+    private fun getVal(refDB: DatabaseReference, ID: String, key: String): String {
+        var value: String? = null
         refDB.child(key).child(ID)
             .addValueEventListener(object : ValueEventListener {
                 override fun onDataChange(snapshot: DataSnapshot) {
@@ -41,6 +44,7 @@ class MainActivity : AppCompatActivity() {
                                     "TAG",
                                     "" + snapshot.value
                                 ) // your name values you will get here
+                                value = snapshot.value!!.toString()
                             } catch (e: Exception) {
                                 e.printStackTrace()
                             }
@@ -56,5 +60,6 @@ class MainActivity : AppCompatActivity() {
                     Log.e("onCancelled", " cancelled")
                 }
             })
+        return value.toString()
     }
 }
